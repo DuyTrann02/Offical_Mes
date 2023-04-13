dacbab5aa143681ea63ad979bbe58ca899028f1a
Antonio Tran <duygnar@gmail.com>
2023-04-13 15:59:48 +0700

    update

--- src/components/SingleBoiCanh.js
+++ src/components/SingleBoiCanh.js
@@ -7,7 +7,7 @@ function SingleBoiCanh(props){
   
     return(
         <>
-        <Box sx ={{width:"70vw", height:"30vh", background:"rgba(147,112,219,0.4)", border:"1px solid black", display:"flex"}}>
+        <Box sx ={{width:"70vw", minHeight:"30vh", background:"rgba(147,112,219,0.4)", border:"1px solid black", display:"flex"}}>
             <Box sx = {{width:"40vw",height:"30vh"}}>
                 <Box sx={{width:"28vw", height:"37px", backgroundColor:"black", clipPath:"polygon(0% 0%, 100% 0%, 100% 52%, 92% 100%, 0% 100%)", padding:"3px"}}>
                    <Box sx={{width:"27.9vw", height:"36px", backgroundColor:"white", clipPath:"polygon(0% 0%, 100% 0%, 100% 50%, 93% 100%, 0% 100%)", display:"flex", justifyContent:"space-around",alignItems:"center"}}>
